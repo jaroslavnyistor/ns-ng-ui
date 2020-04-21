f9606a2deac5c0b43b47393b56a6e9f5a589bba7
Jaroslav Nyistor <jn.nikisoft@gmail.com>
2020-04-21 16:08:31 +0200

    Service became part of autocomplete configuration

--- form/controls/autocomplete/ns-form-control-autocomplete.configuration.ts
+++ form/controls/autocomplete/ns-form-control-autocomplete.configuration.ts
@@ -1,5 +1,7 @@
 import { NsFormControlConfiguration } from '../ns-form-control.configuration';
+import { NsFormControlAutocompleteService } from './ns-form-control-autocomplete.service';
 
 export interface NsFormControlAutocompleteConfiguration extends NsFormControlConfiguration {
    textProperty: string;
+   service: NsFormControlAutocompleteService<any>
 }
