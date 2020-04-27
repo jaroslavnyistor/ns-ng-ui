912a2e308e511029104d25bb8fc4d8b60cc448b1
Jaroslav Nyistor <jn.nikisoft@gmail.com>
2020-04-27 09:55:33 +0200

    Improved not found and no permission components

--- page/no-permission/ns-page-no-permission.module.ts
+++ page/no-permission/ns-page-no-permission.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
 import { LocalizationLanguagesModule } from '../../localization/languages/localization-languages.module';
 import { NsPageNoPermissionRoutingModule } from './ns-page-no-permission-routing.module';
 import { NsPageNoPermissionComponent } from './ns-page-no-permission.component';
@@ -13,7 +14,8 @@ import { NsPageNoPermissionComponent } from './ns-page-no-permission.component';
       CommonModule,
       FlexModule,
       LocalizationLanguagesModule,
-      NsPageNoPermissionRoutingModule
+      NsPageNoPermissionRoutingModule,
+      MatCardModule
    ],
    exports: [
       NsPageNoPermissionComponent
--- page/not-found/ns-page-not-found.module.ts
+++ page/not-found/ns-page-not-found.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
 import { LocalizationLanguagesModule } from '../../localization/languages/localization-languages.module';
 import { NsPageNotFoundRoutingModule } from './ns-page-not-found-routing.module';
 import { NsPageNotFoundComponent } from './ns-page-not-found.component';
@@ -14,6 +15,7 @@ import { NsPageNotFoundComponent } from './ns-page-not-found.component';
       FlexModule,
       LocalizationLanguagesModule,
       NsPageNotFoundRoutingModule,
+      MatCardModule,
    ],
    exports: [
       NsPageNotFoundComponent
