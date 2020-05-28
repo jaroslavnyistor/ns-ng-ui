2ea4b7ebe1c6c3d6b53dc049665084313bb03e31
NikiSoft <jn.nikisoft@gmail.com>
2020-05-28 15:55:55 +0200

    Fixed public api of lib

--- projects/ns-ng-ui/src/public-api.ts
+++ projects/ns-ng-ui/src/public-api.ts
@@ -4,8 +4,8 @@
  * To generate all public-api as one index.ts do following:
  * 1. npm install create-ts-index --save-dev
  * 2. Open terminal at src folder of library
- * 3. Run cti entrypoint -n ./lib
+ * 3. Run cti entrypoint -n .
  * 4. Rename file entrypoint.ts to index.ts
  * 5. Added here export * from './lib/index';
  */
-export * from './lib/index';
+export * from './index';
