c6791c8dcecfc46bd6bdf733a03344d442557c27
NikiSoft <jn.nikisoft@gmail.com>
2020-06-11 16:51:59 +0200

    App - Renamed forms-array.can-delete-customers.model to forms-array-can-delete-customers.model

--- projects/nikisoft-ng-ui-playground/src/app/modules/forms-array/basic/forms-array-can-delete-customers.model.ts
+++ projects/nikisoft-ng-ui-playground/src/app/modules/forms-array/basic/forms-array-can-delete-customers.model.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { NsFormControlCheckboxModel } from '../../../../../../nikisoft-ng-ui/src/lib/form/controls/checkbox/ns-form-control-checkbox.model';
-import { FormsArrayCustomersArrayModel } from '../customers/forms-array.customers-array.model';
+import { FormsArrayCustomersArrayModel } from '../customers/forms-array-customers-array.model';
 import { FormsArrayBasicEntity } from './forms-array-basic.entity';
 
 @Injectable()
