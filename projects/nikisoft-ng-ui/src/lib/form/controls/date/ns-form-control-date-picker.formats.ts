8a96f0355fec1728f3cd7d330c4346b660144ad2
NikiSoft <jn.nikisoft@gmail.com>
2020-06-11 16:05:47 +0200

    Lib - NsFormControlDatePicker has its own date formats

---
+++ projects/nikisoft-ng-ui/src/lib/form/controls/date/ns-form-control-date-picker.formats.ts
@@ -0,0 +1,29 @@
+import { Provider } from '@angular/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { DateAdapter } from '@angular/material/core';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { NsLanguage } from 'nikisoft-utils';
+
+const NS_DATE_PICKER_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'LL',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
+export function getNsDatePickersProviders(): Provider[] {
+  return [
+    { provide: MAT_DATE_LOCALE, useFactory: NsLanguage.resolve },
+    // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
+    // `MatMomentDateModule` in your applications root module. We provide it at the component level
+    // here, due to limitations of our example generation script.
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: NS_DATE_PICKER_FORMATS },
+  ];
+}
